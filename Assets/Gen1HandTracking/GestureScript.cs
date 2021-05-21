c0c9229cbcff58b1a71a675747370d66087aada0
xuefeng1123 <1827699942@qq.com>
2021-05-21 22:37:13 +0800

    add some test script

---
+++ Assets/Gen1HandTracking/GestureScript.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GestureScript : MonoBehaviour,IMixedRealityGestureHandler
+{
+    public void OnGestureCanceled(InputEventData eventData)
+    {
+        text.GetComponent<TextMeshPro>().text = "OnGestureCanceled" ;
+    }
+
+    public void OnGestureCompleted(InputEventData eventData)
+    {
+        text.GetComponent<TextMeshPro>().text = "OnGestureCompleted" ;
+    }
+
+    public void OnGestureStarted(InputEventData eventData)
+    {
+        text.GetComponent<TextMeshPro>().text = "OnGestureStarted" ;
+    }
+
+    public void OnGestureUpdated(InputEventData eventData)
+    {
+        text.GetComponent<TextMeshPro>().text = "OnGestureUpdated" ;
+    }
+
+    GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("handText");
+        text.GetComponent<TextMeshPro>().text = "start" ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
