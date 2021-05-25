96822051ed8dce92ea58fa9c18a5dacbeb28d761
xuefeng1123 <1827699942@qq.com>
2021-05-25 16:36:51 +0800

    separate two scripts to handle hand events and joints events respectively

---
+++ Assets/Hololens2HandTracking/HandTrackingScript.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HandTrackingScript : MonoBehaviour, IMixedRealitySourceStateHandler
+{
+
+    GameObject handsText;
+    IMixedRealityHand hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handsText = GameObject.Find("handsText");
+        handsText.GetComponent<TextMeshPro>().text = "start";
+
+    }
+    void Update()
+    {
+        if (hand != null)
+        {
+            handsText.GetComponent<TextMeshPro>().text = "Source detected: " + hand.ControllerHandedness + "\n";
+            handsText.GetComponent<TextMeshPro>().text += "Velocity " + hand.Velocity + "\n";
+            handsText.GetComponent<TextMeshPro>().text += "is pointing? " + hand.IsInPointingPose + "\n";
+
+            if (hand.TryGetJoint(TrackedHandJoint.IndexTip, out MixedRealityPose jointPose))
+            {
+                handsText.GetComponent<TextMeshPro>().text += "Got joint " + jointPose.ToString(); 
+            }
+        }
+    }
+    private void OnEnable()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealitySourceStateHandler>(this);
+    }
+
+    private void OnDisable()
+    {
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealitySourceStateHandler>(this);
+    }
+
+
+    public void OnSourceDetected(SourceStateEventData eventData)
+    {
+        hand = eventData.Controller as IMixedRealityHand;
+    }
+
+    public void OnSourceLost(SourceStateEventData eventData)
+    {
+        // Only react to articulated hand input sources
+        if (hand != null)
+        {
+            handsText.GetComponent<TextMeshPro>().text = "Source lost: " + hand.ControllerHandedness;
+        }
+        hand = null;
+    }
+
+
+
+}
---
+++ Assets/Hololens2HandTracking/JointTrackingScript.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class JointTrackingScript : MonoBehaviour, IMixedRealityHandJointHandler
+{
+    // Start is called before the first frame update
+    GameObject jointsText;
+    void Start()
+    {
+        jointsText = GameObject.Find("jointsText");
+        jointsText.GetComponent<TextMeshPro>().text = "start";
+    }
+    private void OnEnable()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealityHandJointHandler>(this);
+    }
+
+    private void OnDisable()
+    {
+        CoreServices.InputSystem?.UnregisterHandler<IMixedRealityHandJointHandler>(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out MixedRealityPose pose))
+        //{
+        //    text.GetComponent<TextMeshPro>().text = pose.Position.ToString();
+        //}
+    }
+    public void OnHandJointsUpdated(InputEventData<IDictionary<TrackedHandJoint, MixedRealityPose>> eventData)
+    {
+        jointsText.GetComponent<TextMeshPro>().text = "Hand: " + eventData.Handedness + "\n";
+        int i = 0;
+        foreach (TrackedHandJoint joint in eventData.InputData.Keys)
+        {
+            MixedRealityPose pose;
+            eventData.InputData.TryGetValue(joint, out pose);
+            jointsText.GetComponent<TextMeshPro>().text += "joint " + (i++).ToString() + "  " + joint.ToString().Trim().PadRight(25) + "  Position: " + pose.Position.ToString() + "  Rotation: " + pose.Rotation.ToString() + "\n";
+        }
+
+    }
+}
